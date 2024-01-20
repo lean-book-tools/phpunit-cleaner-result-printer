db4b38c8bf7272393477aa9d05817ed7c5e12cd4
Tomas Votruba <tomas.vot@gmail.com>
2024-01-20 14:34:46 +0100

    cs

--- src/OutputCleaner.php
+++ src/OutputCleaner.php
@@ -4,45 +4,20 @@ declare(strict_types=1);
 
 namespace LeanBookTools;
 
-use LeanBookTools\Naming\ClassNaming;
-
 /**
  * @see \LeanBookTools\Test\OutputCleanerTest
  */
 final class OutputCleaner
 {
-    // Override the line length, to make the output fit the book pages
+    // @todo Override the line length, to make the output fit the book pages
     // parent::__construct($out, $verbose, $colors, $debug, 70, false);
-
-    //    /**
-    //     * @todo use somewhere
-    //     */
-    //    public static function cleanUpTestName(string $testName): string
-    //    {
-    //        $result = preg_match('/^(.+Test)::/', $testName, $matches);
-    //        if ($result === 0) {
-    //            return $testName;
-    //        }
-    //
-    //        $testClass = $matches[1];
-    //
-    //        $simpleClassName = ClassNaming::resolveShortClassName($testClass);
-    //
-    //        // Strip the namespace from the test class
-    //        $testName = str_replace($testClass, $simpleClassName, $testName);
-    //
-    //        // Remove dump of data set (e.g. SmartFileInfo)
-    //        /** @var string */
-    //        $testName = preg_replace('/(data set #(\d+)).+/', '$1', $testName);
-    //
-    //        return trim($testName, ' ');
-    //    }
 
     public static function cleanUpExceptionMessage(string $message): string
     {
         // remove directory from file paths
         $lines = explode("\n", $message);
         foreach ($lines as $key => $line) {
+            // remove the line with vendor/phpunit reference, not needed
             if (str_contains($line, 'vendor/phpunit')) {
                 unset($lines[$key]);
                 continue;
