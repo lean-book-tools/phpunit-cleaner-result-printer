f41421077816ebd96c0741e6169a4cfd1e7a167b
Matthias Noback <matthiasnoback@gmail.com>
2024-01-31 09:52:12 +0100

    Hide PHPUnit version number
    Fixes #8

--- src/Subscribers/TestRunner/TestRunnerStartedSubscriber.php
+++ src/Subscribers/TestRunner/TestRunnerStartedSubscriber.php
@@ -7,13 +7,12 @@ namespace LeanBookTools\Subscribers\TestRunner;
 use LeanBookTools\Subscribers\AbstractSubscriber;
 use PHPUnit\Event\TestRunner\Started;
 use PHPUnit\Event\TestRunner\StartedSubscriber;
-use PHPUnit\Runner\Version;
 
 final class TestRunnerStartedSubscriber extends AbstractSubscriber implements StartedSubscriber
 {
     public function notify(Started $event): void
     {
         // starting message is printed by PHPUnit itself
-        $this->simplePrinter->writeln('PHPUnit ' . Version::id() . PHP_EOL);
+        $this->simplePrinter->writeln('PHPUnit' . PHP_EOL);
     }
 }
